3c9bc1c236b1e0f20d08df09faf1179fe89847d2
veerapat <Weeraphatnakub1150@gmail.com>
2026-03-07 18:53:48 +0700

    first commit flutter web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "769838503"
+    serviceWorkerVersion: "2291031472"
   }
 });
